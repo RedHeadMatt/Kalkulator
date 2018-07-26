40ba5bc6b239a0bac246f05b77589b655f776e66
RedHeadMatt <m.k.franczak@gmail.com>
2018-07-26 18:23:15 +0200

    FIRST COMMIT MAYBE

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculatorrr {
+
+    double a;
+    double b;
+
+
+
+    public double add(double a, double b){
+        return a+b;
+    }
+
+    public double substract(double a, double b){
+        return a-b;
+    }
+}
